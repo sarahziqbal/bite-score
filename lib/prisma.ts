639853f7f70474b7415f3881eadf46c7ed862076
fruitlesspeak <p.jhanji22@gmail.com>
2025-12-30 20:04:16 -0600

    added single prisma connection and updated dependencies

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
 
-export { prisma }
+const adapter = new PrismaPg({ connectionString });
+
+export const prisma = new PrismaClient({ adapter });
